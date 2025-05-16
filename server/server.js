6cf284cd37adff940190732a9d41af450cb264b2
Ragul198 <ragul198a@gmail.com>
2025-05-16 19:48:49 +0530

    1

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use('/api/messages', messageRouter);
 //Listen to requests
 if(process.env.NODE_ENV !== "production"){ 
     const PORT = process.env.PORT || 5000
-server.listen(PORT, () => console.log("Server is running on port :"+ PORT));
+    server.listen(PORT, () => console.log("Server is running on port :"+ PORT));
     
 }
 //export the server for vercel
